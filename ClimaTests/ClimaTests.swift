977f06a9617a590a4cc9705c0ae378952d759003
Aiaz <aizmyza19961906@gmail.com>
2026-03-09 23:13:23 +0300

    Initial Commit

---
+++ ClimaTests/ClimaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaTests.swift
+//  ClimaTests
+//
+//  Created by Muzafarov on 09.03.2026.
+//
+
+import Testing
+@testable import Clima
+
+struct ClimaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
